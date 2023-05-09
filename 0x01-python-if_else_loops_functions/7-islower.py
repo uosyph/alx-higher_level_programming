4998f0fd92404b9725cc946f7c2127ec2b601a73
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-09 19:42:41 +0300

    fixes

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    if c != c.lower:
-        return True
-    else:
-        return False
+    return c != c.lower()
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,4 +2,4 @@
 def uppercase(str):
     for char in str:
         print("{}".format(chr(ord(char) - 32 if 97 <=
-              ord(char) <= 122 else ord(char))), end="")
+              ord(char) <= 122 else ord(char))))
