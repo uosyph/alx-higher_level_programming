fedc45e234fe68fdf5c918ed9e9e08041eed6776
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-09 20:51:56 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i+1, 10):
-        print("{0}{1}, ".format(i, j) if i == 0 and j == 1
-              else "{0}{1}, ".format(i, j), end="")
+        if i == 0 and j == 1:
+            print("{0}{1}".format(i, j), end="")
+        else:
+            print(", {0}{1}".format(i, j), end="")
+print()
