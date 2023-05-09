c930375b16a1e115d0d65b1baf378f25d3cbff33
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-09 19:33:19 +0300

    minor fixes

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c == c.lower:
+    if c != c.lower:
         return True
     else:
         return False
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        print("{}".format(''.join([chr(ord(char))])), end="")
+        print("{}".format(chr(ord(char) - 32 if 97 <=
+              ord(char) <= 122 else ord(char))), end="")
