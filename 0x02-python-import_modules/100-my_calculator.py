3a7d61d8e2fa72610509e73dd4e8af3cfca67012
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-11 16:24:32 +0300

    0x02 - 100,101,102,103

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    match operator:
+        case "+":
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+            exit()
+        case "-":
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+            exit()
+        case "*":
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+            exit()
+        case "/":
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+            exit()
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
