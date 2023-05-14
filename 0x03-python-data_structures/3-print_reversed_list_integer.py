9f795f525f37f81313363f53f8b2b17389dad2e9
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-14 15:16:04 +0300

    fixes & optimization

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+    if not my_list:
+        return
+    for i in reversed(my_list):
+        print("{:d}".format(i))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,5 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0 or idx > len(my_list):
         return new_list
-    for i in new_list:
-        if i == idx:
-            new_list[i] = element
-            return new_list
+    new_list[idx] = element
+    return new_list
