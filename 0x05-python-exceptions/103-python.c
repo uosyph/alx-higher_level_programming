0edeb8dea338e31c4cd677320ab7b321c11a6185
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-29 20:29:19 +0300

    changes

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -2,10 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_char(const char *str, int n);
 void print_python_float(PyObject *p);
 void print_python_bytes(PyObject *p);
 void print_python_list(PyObject *p);
-void print_hexn(const char *str, int n);
+
+/**
+ * print_python_list - loops over string and prints every character.
+ * @str: the string.
+ * @n: counter.
+ * Return: void.
+ */
+void print_char(const char *str, int n)
+{
+    int i = 0;
+    for (; i < n - 1; ++i)
+    {
+        printf("%02x ", (unsigned char)str[i]);
+    }
+
+    printf("%02x", str[i]);
+    fflush(stdout);
+}
 
 /**
  * print_python_list - prints information about Python lists.
@@ -14,7 +32,7 @@ void print_hexn(const char *str, int n);
  */
 void print_python_list(PyObject *p)
 {
-    int i = 0, list_len = 0;
+    int list_length = 0, i = 0;
     PyObject *item;
     PyListObject *clone = (PyListObject *)p;
 
@@ -25,19 +43,23 @@ void print_python_list(PyObject *p)
         return;
     }
 
-    list_len = PyList_GET_SIZE(p);
-    printf("[*] Size of the Python List = %d\n", list_len);
+    list_length = PyList_GET_SIZE(p);
+    printf("[*] Size of the Python List = %d\n", list_length);
     printf("[*] Allocated = %d\n", (int)clone->allocated);
 
-    for (; i < list_len; ++i)
+    for (; i < list_length; ++i)
     {
         item = PyList_GET_ITEM(p, i);
         printf("Element %d: %s\n", i, item->ob_type->tp_name);
 
         if (PyBytes_Check(item))
+        {
             print_python_bytes(item);
+        }
         else if (PyFloat_Check(item))
+        {
             print_python_float(item);
+        }
     }
 
     fflush(stdout);
@@ -69,7 +91,7 @@ void print_python_bytes(PyObject *p)
     printf("  size: %d\n", clone_size);
     printf("  trying string: %s\n", clone->ob_sval);
     printf("  first %d bytes: ", calc_bytes);
-    print_hexn(clone->ob_sval, calc_bytes);
+    print_char(clone->ob_sval, calc_bytes);
     printf("\n");
 
     fflush(stdout);
@@ -83,39 +105,26 @@ void print_python_bytes(PyObject *p)
 void print_python_float(PyObject *p)
 {
     PyFloatObject *clone = (PyFloatObject *)p;
-    float n = 0;
+    float number = 0;
 
     printf("[.] float object info\n");
+
     if (!PyFloat_Check(p))
     {
         printf("  [ERROR] Invalid Float Object\n");
         return;
     }
 
-    n = clone->ob_fval;
+    number = clone->ob_fval;
 
-    if ((int)n == n)
-        printf("  value: %0.1f\n", clone->ob_fval);
-    else
-        printf("  value: %0.16g\n", clone->ob_fval);
-
-    fflush(stdout);
-}
-
-/**
- * print_python_list - loops over string and prints every character.
- * @str: the string.
- * @n: counter.
- * Return: void.
- */
-void print_hexn(const char *str, int n)
-{
-    int i = 0;
-    for (; i < n - 1; ++i)
+    if ((int)number == number)
     {
-        printf("%02x ", (unsigned char)str[i]);
+        printf("  value: %0.1f\n", clone->ob_fval);
+    }
+    else
+    {
+        printf("  value: %0.16g\n", clone->ob_fval);
     }
 
-    printf("%02x", str[i]);
     fflush(stdout);
 }
