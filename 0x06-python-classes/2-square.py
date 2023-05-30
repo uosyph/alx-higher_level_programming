ceebd2a0f6597deddf8d08aca275914b5ba192d5
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-30 17:46:34 +0300

    another minor change in docstring

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Defines square with size that must be type int and >= 0"""
+"""Defines size that must be type int and >= 0"""
 
 
 class Square:
     """A class that defines a square"""
 
     def __init__(self, size=0):
-        """Initializes Square with size,
-        and checks type and value and raise error accordingly"""
+        """Initializes Square with size"""
         self.__size = size
         if type(size) is not int:
             raise TypeError('size must be an integer')
