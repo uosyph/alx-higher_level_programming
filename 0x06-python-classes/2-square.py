9f803bf512c335ed9803e8ce886d0e54d1a6e14c
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-30 17:51:45 +0300

    minor change

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,10 +7,10 @@ class Square:
     Args: size (int): value of the size"""
 
     def __init__(self, size=0):
-        """Initializes Square with size"""
-        self.__size = size
         """Checks type and value and raise error accordingly"""
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        """Initializes Square with size"""
+        self.__size = size
