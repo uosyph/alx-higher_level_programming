cf74aeb5ecce2897ee8bfd9ac27ca8aae091a006
Yousef Saeed <yousafesaeed@gmail.com>
2023-06-04 19:08:14 +0300

    0x07 - 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Prints square by size input.
+
+Functions:
+    print_square(size)
+
+Raises:
+    TypeError: An error occurred accessing the variable type.
+    ValueError: An error occurred accessing the variable value.
+"""
+
+
+def print_square(size):
+    """prints square '#' by size variable.
+
+    Raises:
+        TypeError: An error occurred accessing the variable type.
+        ValueError: An error occurred accessing the variable value.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    for _ in range(size):
+        for _ in range(size):
+            print("#", end="")
+        print()
