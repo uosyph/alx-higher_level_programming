35f4818e4265d1a5782e5d147f1f3d53c20305d3
Yousef Saeed <yousafesaeed@gmail.com>
2023-06-04 23:04:20 +0300

    minor change

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -22,34 +22,28 @@ def matrix_divided(matrix, div):
         new_matrix: The new divided matrix
     """
 
-    # Check if matrix is a list
     if type(matrix) is not list:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
 
-    # Check if rows in matrix is list
     for rows in matrix:
         if type(rows) is not list:
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats")
 
-    # Check if length of rows are the same
     for row in matrix:
         if len(matrix[0]) != len(row):
             raise TypeError('Each row of the matrix must have the same size')
 
-    # Check to see if div is zero
     if div == 0:
         raise ZeroDivisionError('division by zero')
 
-    # Check if items in matrix are floats or ints
     for rows in matrix:
         for items in rows:
             if not isinstance(items, (int, float)):
                 raise TypeError(
                     "matrix must be a matrix (list of lists) of integers/floats")
 
-    # Divides elements of a matrix
     new_matrix = []
     for rows in matrix:
         if not isinstance(div, (int, float)):
