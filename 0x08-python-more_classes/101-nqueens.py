734c133a76084b6542a6d60def8638fc607d6c10
Yousef Saeed <yousafesaeed@gmail.com>
2023-06-05 18:50:04 +0300

    revert the revert

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python3
-"""Module is to solve the N-Queens challenge problem"""
+"""Solving the N-Queens challenge problem.
+
+The N queens puzzle is the challenge of placing
+N non-attacking queens on an N×N chessboard.
+
+Usage: ./101-nqueens.py <N>
+    Where N must be an integer greater or equal to 4.
+
+Returns: Every possible solution to the problem.
+    One solution per line.
+    Solutions are not printed in a specific order.
+"""
+
 from sys import argv
 
 
-def checkspot(board, r, c):
+def check_spot(board, r, c):
+    """Checks spots for the board.
+
+    Args:
+        board (list): A board to check.
+        r (int): Row.
+        c (int): Column.
+
+    Returns:
+        0: On success.
+        1: On failure.
+    """
+
     n = len(board) - 1
+
     if board[r][c]:
         return 0
+
     for row in range(r):
         if board[row][c]:
             return 0
+
     i = r
     j = c
     while i > 0 and j > 0:
@@ -17,6 +44,7 @@ def checkspot(board, r, c):
         j -= 1
         if board[i][j]:
             return 0
+
     i = r
     j = c
     while i > 0 and j < n:
@@ -24,34 +52,66 @@ def checkspot(board, r, c):
         j += 1
         if board[i][j]:
             return 0
+
     return 1
 
 
-def initboard(n=4):
-    b = []
-    for r in range(n):
-        b.append([0 for c in range(n)])
-    return b
+def init_board(n=4):
+    """Initializes the board.
+
+    Args:
+        n (int, optional): The number of queens. Defaults to 4.
+
+    Returns:
+        board: The initialized board.
+    """
+
+    board = []
+    for _ in range(n):
+        board.append([0 for _ in range(n)])
+    return board
 
 
-def findsoln(board, row):
+def solve(board, row):
+    """Solves a row in the board.
+
+    Args:
+        board (list): The board.
+        row (int): The row to solve.
+
+    Returns:
+        board: The solved board.
+        None: On failure.
+    """
+
     for col in range(len(board)):
-        if checkspot(board, row, col):
+        if check_spot(board, row, col):
             board[row][col] = 1
+
             if row == len(board) - 1:
-                print(convtosoln(board))
+                print(appl_soln(board))
                 board[row][col] = 0
                 continue
-            if findsoln(board, row + 1):
+            elif solve(board, row + 1):
                 return board
             else:
                 board[row][col] = 0
-    return None
+    return
 
 
-def convtosoln(board):
+def appl_soln(board):
+    """Apply the solution.
+
+    Args:
+        board (list): The board to apply the solution.
+
+    Returns:
+        soln: The solution.
+    """
+
     soln = []
     n = len(board)
+
     for r in range(n):
         for c in range(n):
             if board[r][c]:
@@ -60,22 +120,36 @@ def convtosoln(board):
 
 
 def nqueens(n=4):
+    """Solves row by row in each column.
+
+    Args:
+        n (int, optional): The number of queens. Defaults to 4.
+    """
+
     for col in range(n):
-        board = initboard(n)
+        board = init_board(n)
         board[0][col] = 1
-        findsoln(board, 1)
+        solve(board, 1)
 
 
-if __name__ == "__main__":
+def main():
+    """Validates arguments and start solving.
+    """
+
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
     try:
         n = int(argv[1])
-    except:
+    except ValueError:
         print("N must be a number")
         exit(1)
     if n < 4:
         print("N must be at least 4")
         exit(1)
+
     nqueens(n)
+
+
+if __name__ == "__main__":
+    main()
