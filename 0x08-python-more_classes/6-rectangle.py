dad25dc280bdcc4012108410a2a086ec1b6e41a4
Yousef Saeed <yousafesaeed@gmail.com>
2023-06-05 17:20:06 +0300

    fix

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -39,6 +39,7 @@ class Rectangle:
         return f"Rectangle({self.width}, {self.height})"
 
     def __del__(self):
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
     @property
