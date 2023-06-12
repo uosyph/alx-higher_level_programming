78b28fe88d470bb544cf9aea6af4f515253d8b7f
Yousef Saeed <yousafesaeed@gmail.com>
2023-06-12 22:09:14 +0300

    docstring change

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Module contains a function that returns True if the object
-is an instance of a class that inherited (directly or indirectly)
-from the specified class; otherwise False."""
+"""Checks if object is inherits from a class."""
 
 
 def inherits_from(obj, a_class):
