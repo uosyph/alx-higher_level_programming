70b6d85cd72ff8e2122d7f500e10aaaf3561d5d0
Yousef Saeed <yousafesaeed@gmail.com>
2023-06-14 01:45:39 +0300

    minor changes

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,40 +4,40 @@
 from sys import stdin
 
 
-def print_stats(size, status_codes):
+def print_stats(file_size, status_codes):
     """Prints final metrics"""
-    print("File size: {}".format(size))
+    print("File file_size: {}".format(file_size))
     for code in sorted(status_codes):
         print(f"{code}: {status_codes[code]}")
 
 
 # Initialize variables to keep track of metrics
-size = 0
+file_size = 0
 status_codes = {}
-valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-count = 0
+valid_status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+counter = 0
 
 try:
     # Read input line by line
     for line in stdin:
         # Update metrics
-        if count == 10:
+        if counter == 10:
             # Print metrics every 10 lines
-            print_stats(size, status_codes)
-            count = 1
+            print_stats(file_size, status_codes)
+            counter = 1
         else:
-            count += 1
+            counter += 1
 
         # Parse the line to extract file size
         line = line.split()
         try:
-            size += int(line[-1])
+            file_size += int(line[-1])
         except (IndexError, ValueError):
             pass
 
         # Parse the line to extract status code
         try:
-            if line[-2] in valid_codes:
+            if line[-2] in valid_status_codes:
                 if status_codes.get(line[-2], -1) == -1:
                     status_codes[line[-2]] = 1
                 else:
@@ -46,9 +46,9 @@ try:
             pass
 
     # Print final metrics
-    print_stats(size, status_codes)
+    print_stats(file_size, status_codes)
 
 except KeyboardInterrupt:
     # Print final metrics on keyboard interrupt
-    print_stats(size, status_codes)
+    print_stats(file_size, status_codes)
     raise
