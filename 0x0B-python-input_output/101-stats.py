266a045ceb424c09f11eb48c3e2ebbaa354114bc
Yousef Saeed <yousafesaeed@gmail.com>
2023-06-14 01:15:28 +0300

    revert back

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,43 +1,36 @@
 #!/usr/bin/python3
 """A script that reads stdin line by line and computes metrics"""
 
+from sys import stdin
 
-import sys
-
-TOTAL_FILE_SIZE = 0
-STATUS_CODES = {200: 0, 301: 0, 400: 0, 401: 0,
+# Initialize variables to keep track of metrics
+total_file_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
                 403: 0, 404: 0, 405: 0, 500: 0}
-LINES_PROCESSED = 0
 
 try:
-    for line in sys.stdin:
-        try:
-            ip_address, _, _, timestamp, _, request, \
-                status_code, file_size = line.split()
-        except ValueError:
-            continue
-        if request != 'GET /projects/260 HTTP/1.1':
-            continue
-        try:
-            file_size = int(file_size)
-            status_code = int(status_code)
-        except ValueError:
-            continue
-        LINES_PROCESSED += 1
-        TOTAL_FILE_SIZE += file_size
-        if status_code in STATUS_CODES:
-            STATUS_CODES[status_code] += 1
-        if LINES_PROCESSED % 10 == 0:
-            print(f"File size: {TOTAL_FILE_SIZE}")
-            for code in sorted(STATUS_CODES.keys()):
-                if STATUS_CODES[code] > 0:
-                    print(f"{code}: {STATUS_CODES[code]}")
+    # Read input line by line
+    for i, line in enumerate(stdin):
+        # Parse the line to extract file size and status code
+        parts = line.split()
+        file_size = int(parts[-1])
+        status_code = int(parts[-2])
+
+        # Update metrics
+        total_file_size += file_size
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+
+        # Print metrics every 10 lines
+        if (i+1) % 10 == 0:
+            print(f"Total file size: {total_file_size}")
+            for code in sorted(status_codes.keys()):
+                if status_codes[code] > 0:
+                    print(f"{code}: {status_codes[code]}")
 
 except KeyboardInterrupt:
-    pass
-
-finally:
-    print(f"File size: {TOTAL_FILE_SIZE}")
-    for code in sorted(STATUS_CODES.keys()):
-        if STATUS_CODES[code] > 0:
-            print(f"{code}: {STATUS_CODES[code]}")
+    # Print final metrics on keyboard interrupt
+    print(f"Total file size: {total_file_size}")
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
