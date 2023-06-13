eed4dfa56ace03d9a5a1184f61cb6ed9a7a27b72
Yousef Saeed <yousafesaeed@gmail.com>
2023-06-14 00:00:26 +0300

    revert the revert

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,19 +2,17 @@
 """Adds all arguments to a list, then save them to a file"""
 
 import sys
-from os import path
 
 savefile = __import__('5-save_to_json_file').save_to_json_file
 loadfile = __import__('6-load_from_json_file').load_from_json_file
 
 
-filename = "add_item.json"
-args_list = []
+args = sys.argv[1:]
 
-if path.exists(filename):
-    args_list = loadfile(filename)
+try:
+    args_list = loadfile("add_item.json")
+except Exception:
+    args_list = []
 
-for arg in sys.argv[1:]:
-    args_list.append(arg)
-
-savefile(args_list, filename)
+args_list += args
+savefile(args_list, "add_item.json")
