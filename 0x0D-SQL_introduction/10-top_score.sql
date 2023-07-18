08967d1b72c75d5ebb31e83c4c9f5a6f59c015da
Yousef Saeed <yousafesaeed@gmail.com>
2023-07-18 16:34:22 +0300

    0x0D - (8-16)

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- Lists the scores and names and orders them by score descending.
+SELECT score, name FROM second_table ORDER by score DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Counts and displays instances of a table where the id is 89.
+SELECT COUNT(`id`) FROM first_table WHERE id = 89;
