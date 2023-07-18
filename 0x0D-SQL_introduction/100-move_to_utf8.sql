7f9edd50ca515c0891b64c3ac8d1237896e711bf
Yousef Saeed <yousafesaeed@gmail.com>
2023-07-18 16:50:18 +0300

    minor fixes

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,13 +1,15 @@
--- Converts a database to UTF8 encoding
+-- Converts a database to UTF8 encoding.
+ALTER DATABASE
+    hbtn_0c_0
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER DATABASE hbtn_0111c_0
-      CHARACTER SET = utf8mb4
-      COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE first_table
-      CONVERT TO CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table
-      CHANGE name name
-      VARCHAR(256)
-      CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
+ALTER TABLE
+    first_table
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+ALTER TABLE
+    first_table
+    MODIFY name VARCHAR(256)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
