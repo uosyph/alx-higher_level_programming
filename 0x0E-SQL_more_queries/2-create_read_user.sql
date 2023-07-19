afec714241e7b032076516ecee564218b13a6fbe
Yousef Saeed <yousafesaeed@gmail.com>
2023-07-19 17:32:49 +0300

    minor fixes

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Creates Database if it doesn't already exist, and grants privileges to a certain user.
+-- Creates database if it doesn't already exist, and grants privileges to a certain user.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2 @localhost IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
