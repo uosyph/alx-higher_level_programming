a630d9ecff0c57445c78aa3671e9f0ce00d7e837
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-14 13:57:30 +0300

    0x12 - (0-5)

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const len = process.argv.length;
+if (len < 3) {
+	console.log("No argument");
+} else if (len > 3) {
+	console.log("Arguments found");
+} else {
+	console.log("Argument found");
+}
