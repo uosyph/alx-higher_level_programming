f2ddf886e47182b4d45a93b6f2a576b5a7cdb965
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-14 14:27:41 +0300

    fix styling

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const len = process.argv.length;
 if (len < 3) {
-	console.log("No argument");
+	console.log('No argument');
 } else if (len > 3) {
-	console.log("Arguments found");
+	console.log('Arguments found');
 } else {
-	console.log("Argument found");
+	console.log('Argument found');
 }
