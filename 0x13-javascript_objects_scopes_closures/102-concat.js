8f3b8ce179c3bb8c048e8d5d27c3a4a2947203af
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-16 01:08:32 +0300

    0x13 - 10(0-2)

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const fileSystem = require('fs');
+const firstArg = fileSystem.readFileSync(process.argv[2]).toString();
+const secondArg = fileSystem.readFileSync(process.argv[3]).toString();
+fileSystem.writeFileSync(process.argv[4], firstArg + secondArg);
