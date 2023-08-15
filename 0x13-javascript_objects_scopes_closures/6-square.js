8655365329d8ff34f0bbe468e7cfae8cac59b1dc
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-16 01:07:55 +0300

    0x13 - (6-10)

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const newSquare = require('./5-square');
+
+class Square extends newSquare {
+  charPrint (c) {
+    for (let count = 0; count < this.height; count++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
