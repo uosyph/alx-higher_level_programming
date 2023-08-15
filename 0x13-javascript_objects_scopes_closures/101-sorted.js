9ec22db016e03eb7d50589fc0f6cb6fdd961ccd4
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-16 01:16:51 +0300

    Revert recent change

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 const newDict = {};
-Object.keys(dict).map(function (key) {
+Object.keys(dict).map(function (key, index) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }
