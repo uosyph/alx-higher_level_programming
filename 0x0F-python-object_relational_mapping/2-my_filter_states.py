d2957538f5720bc58de229cafa56062bb1e82769
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-19 00:59:26 +0300

    Another format change

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,14 +11,11 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute(f'SELECT *\
-                FROM states\
-                WHERE states.name LIKE BINARY "{str(argv[4])}"\
-                ORDER BY states.id ASC')
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%{}%'\
+    ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
-    if rows:
-        for row in rows:
-            print(f"{row}")
+    for row in rows:
+        print(row)
 
     cur.close()
     db.close()
