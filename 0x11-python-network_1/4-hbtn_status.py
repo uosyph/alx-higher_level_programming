d542e33f6bc16b0fb19f9ade1446cf2c3550d632
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-02 00:36:49 +0300

    Separate print()

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     from requests import get
 
     req = get('https://intranet.hbtn.io/status')
-    print(
-        f"Body response:\n\t- type: {type(req.text)}\n\t- content: {req.text}"
-        .expandtabs(4))
+    print("Body response:")
+    print(f"    - type: {type(req.text)}")
+    print(f"    - content: {req.text}")
