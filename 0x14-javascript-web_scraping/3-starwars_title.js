be3ce75ee9b791e64d13b69bafb6feff4c934a89
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-26 16:11:29 +0300

    Fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,9 +2,9 @@
 
 const request = require('request');
 
-const files = process.argv.slice(2);
-request('https://swapi-api.alx-tools.com/api/films/' + files[0],
-  function (err, body) {
+const id = process.argv.slice(2);
+request('https://swapi-api.alx-tools.com/api/films/' + id[0],
+  function (err, res, body) {
     if (err) console.error('error:', err);
     else console.log(JSON.parse(body).title);
   });
