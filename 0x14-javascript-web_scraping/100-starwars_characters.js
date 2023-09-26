d9c953f96f4453e030fb8e9fec2cd7d492229608
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-26 19:53:48 +0300

    0x14 - 100,101

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+let data;
+const movieId = process.argv.slice(2)[0];
+request('https://swapi-api.alx-tools.com/api/films/' + movieId,
+  function (err, res, body) {
+    if (err) console.error(err);
+    else {
+      data = JSON.parse(body);
+      for (const character in data.characters) {
+        request(data.characters[character], function (err, res, body) {
+          if (err) console.error(err);
+          else console.log(JSON.parse(body).name);
+        });
+      }
+    }
+  });
---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+let data;
+let characters = [];
+const movieId = process.argv.slice(2)[0];
+
+function getCharacterName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) reject(err);
+      else resolve(JSON.parse(body).name);
+    });
+  });
+}
+
+function getCharacterId (movieId) {
+  request('https://swapi-api.alx-tools.com/api/films/' + movieId,
+    async function (err, res, body) {
+      if (err) console.error(err);
+      else {
+        data = JSON.parse(body);
+        characters = data.characters;
+        for (let id = 0; id < characters.length; id++) {
+          const characterName = await getCharacterName(characters[id]);
+          console.log(characterName);
+        }
+      }
+    });
+}
+
+getCharacterId(movieId);
