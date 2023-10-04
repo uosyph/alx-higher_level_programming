8ebc634130d8a61cb153599f3df273f15fbacb10
Yousef Saeed <yousafesaeed@gmail.com>
2023-10-04 07:02:00 +0300

    0x15 - 9,102,103

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const $ = window.$;
+function getHello () {
+  $.ajax({
+    type: 'GET',
+    data: {
+      lang: $('INPUT#language_code').val()
+    },
+    url: 'https://fourtonfish.com/hellosalut/',
+    success: function (data) {
+      $('DIV#hello').text(data.hello);
+    }
+  });
+}
+
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    getHello();
+  });
+
+  $('INPUT#language_code').keypress(function (event) {
+    if (event.which === 13) {
+      getHello();
+    }
+  });
+});
